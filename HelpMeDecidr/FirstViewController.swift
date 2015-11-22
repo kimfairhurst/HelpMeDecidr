344ec123d8572b8b941cf57aba39c6b6f792e7f7
Jon Wong <kimberlyfairhurst@kimberlys-macbook-pro.local>
2015-11-22 12:31:12 -0800

    Initial Commit

---
+++ HelpMeDecidr/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  HelpMeDecidr
+//
+//  Created by Kimberly Fairhurst on 11/22/15.
+//  Copyright © 2015 Kim Fairhurst. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
